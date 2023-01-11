41f4e26e9162e9834814b6685aa055de759ac9fe
nicholasvp <nicholasvpinheiro@gmail.com>
2023-01-11 16:26:07 -0300

    request

--- src/server.js
+++ src/server.js
@@ -2,8 +2,23 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (request, response) => {
-	response.send("Hello, world!");
+app.get("/message/:id/:user", (request, response) => {
+	const { id, user } = request.params
+
+	response.send(`
+	Messagem ID: ${id}.
+	Nome do usuário: ${user}
+	`);
+
+app.get("/users", (request, response) => {
+	const { page, limit } = request.query
+
+	response.send(`
+	Página: ${page}.
+	Mostrar: ${limit}.
+	`)
+})
+
 });
 
 const PORT = 3333;
