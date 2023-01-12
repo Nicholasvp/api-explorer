e883b1580048051c1a654fa85a350b464a95379d
nicholasvp <nicholasvpinheiro@gmail.com>
2023-01-12 16:19:38 -0300

    atualization

--- src/server.js
+++ src/server.js
@@ -1,25 +1,12 @@
 const express = require("express");
 
+// por padrão esse require vai carregar o index
+const routes = require("./routes")
+
 const app = express();
+app.use(express.json());
 
-app.get("/message/:id/:user", (request, response) => {
-	const { id, user } = request.params
-
-	response.send(`
-	Messagem ID: ${id}.
-	Nome do usuário: ${user}
-	`);
-
-app.get("/users", (request, response) => {
-	const { page, limit } = request.query
-
-	response.send(`
-	Página: ${page}.
-	Mostrar: ${limit}.
-	`)
-})
-
-});
+app.use(routes)
 
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
